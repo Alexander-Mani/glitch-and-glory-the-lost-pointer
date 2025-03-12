a22f150dc7a70515967e062a4441a9581292b39f
Alexander-Mani <lexman02@gmail.com>
2025-03-12 08:04:24 +0000

    Some ascii for start of game

--- UILayer/OverworldHandler.cpp
+++ UILayer/OverworldHandler.cpp
@@ -4,6 +4,8 @@
 #include "../Models/AllEntities.h"      // For all entity models
 #include "../Models/BattleModel.h"
 #include "../LogicLayer/EntityLogic.h"    // For EntityLogic
+#include <iostream>
+#include <ostream>
 #include <string>
 
 #include <cassert>
@@ -19,10 +21,25 @@ OverworldHandler::OverworldHandler(LogicWrapper* logicWrapper, AsciiHandler* asc
 }
 
 void OverworldHandler::initialize_overworld() {
-    cout << "Welcome to the overworld!" << endl;
+    this->ioHandler.clear_terminal();
+    string city = this->asciiHandler->CITY;
+    string map = this->asciiHandler->GAME_MAP;
+    string title = this->asciiHandler->TITLE;
+    string subtitle = this->asciiHandler->SUBTITLE;
+    cout << title << endl;
+    cout << subtitle << endl;
+    cout << city << endl;
+    cout << map << endl;
     const std::string boldRed = "\033[1;31m";
     const std::string reset = "\033[0m";
-    string text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+    string text = 
+R"(Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+It has survived not only five centuries, but also the leap into electronic typesetting, 
+remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
+sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like 
+Aldus PageMaker including versions of Lorem Ipsum.)";
      for (const char c : text) {
         std::cout << c << std::flush;
         std::this_thread::sleep_for(std::chrono::milliseconds(50));
